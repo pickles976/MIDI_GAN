3e5d01f3b19375faa7b884997d6616ac6d398020
Sebastian <sebaslogo@gmail.com>
2022-02-28 18:47:58 -0600

    Added full pipeline

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from utils import preProcess
+from utils import preProcess, midiToPng, pngToMidi
+import subprocess
+import os
 
-song = "1943-lev1.mid"
-# song = "PkmGS-Battle1.mid"
+# RAW, PROCESS = "NES_MIDI_RAW","NES_MIDI_PROCESS"
+# PNG, FINAL = "NES_PNG","NES_PNG_MIDI"
 
-newsong = "new_song.mid"
+RAW, PROCESS = "GB_MIDI_RAW","GB_MIDI_PROCESS"
+PNG, FINAL = "GB_PNG","GB_PNG_MIDI"
 
-preProcess(song,newsong)
+
+# pre-process files
+for fn in os.listdir(RAW):
+
+    infile,outfile = os.path.join(RAW,fn), os.path.join(PROCESS,fn)
+    preProcess(infile,outfile)
+
+# MIDI files to PNG
+for fn in os.listdir(PROCESS):
+
+    name = fn.split(".")[0]
+    infile,outfile = os.path.join(PROCESS,fn),os.path.join(PNG,name+".png")
+    midiToPng(infile,outfile)
+
+# PNG back to MIDI
+for fn in os.listdir(PNG):
+
+    name = fn.split(".")[0]
+    infile,outfile = os.path.join(PNG,fn),os.path.join(FINAL,name+".mid")
+    pngToMidi(infile,outfile)
--- utils.py
+++ utils.py
@@ -1,162 +1,196 @@
 from mido import MidiFile, MidiTrack, Message, MetaMessage
 import numpy as np
 import cv2
+import sys
 
 # 128x128 = 16384
 # 256x256 = 65,536
-MIN_NOTE = 64 # smallest possible note, 32nd note
+MIN_NOTE = 32 # smallest possible note, 32nd note
+MIN_VALUE = 500000 # approx length of song
 DIM = 256 
 SIZE = DIM ** 2
 NUM_TRACKS = 4
 
 def preProcess(song,newsong):
 
-    midi = MidiFile(song)
+    print(f"Processing {song}")
 
-    tpb = midi.ticks_per_beat   # normalize the ticks per beat
-    midi.ticks_per_beat = MIN_NOTE
-    div = tpb / MIN_NOTE
+    try:
 
-    # remove duplicate tracks
-    message_numbers = []
-    duplicates = []
+        midi = MidiFile(song)
+        midi.type = 1
 
-    for track in midi.tracks:
-        if len(track) in message_numbers:
-            duplicates.append(track)
-        else:
-            message_numbers.append(len(track))
+        tpb = midi.ticks_per_beat   # normalize the ticks per beat
+        midi.ticks_per_beat = MIN_NOTE
+        div = tpb / MIN_NOTE
 
-    for track in duplicates:
-        midi.tracks.remove(track)
+        # remove duplicate tracks
+        message_numbers = []
+        duplicates = []
 
-    # loop through all midi tracks
-    for track in midi.tracks:
-        
-        print(track)
+        for track in midi.tracks:
+            if len(track) in message_numbers:
+                duplicates.append(track)
+            else:
+                message_numbers.append(len(track))
 
-        i = 0
-        for message in track:
+        for track in duplicates:
+            midi.tracks.remove(track)
 
-            # normalize time, 1 == 32nd note
-            if message.time:
-                message.time = int(message.time / div)
+        # loop through all midi tracks
+        for track in midi.tracks:
+            
+            # print(track)
 
-            # normalize data format
-            if message.type and message.type == "note_off":
-                track[i] = Message('note_on', channel=message.channel,note=message.note, velocity=0, time=message.time)
+            i = 0
+            for message in track:
 
-            i += 1
+                # normalize time, 1 == 32nd note
+                if message.time:
+                    message.time = int(message.time / div)
 
-    # save the midi with normalized timestamps
-    midi.save(newsong)
+                # normalize data format
+                if message.type and message.type == "note_off":
+                    track[i] = Message('note_on', channel=message.channel,note=message.note, velocity=0, time=message.time)
+
+                i += 1
+
+        # save the midi with normalized timestamps
+        midi.save(newsong)
+
+    except:
+        print("Pr-Processing failed!")
 
 def pngToMidi(imgfile,midifile):
 
-    offset = int(SIZE / NUM_TRACKS)
+    print(f"Converting {imgfile} to {midifile}")
 
-    # create midifile
-    midi = MidiFile()
+    try:
 
-    img = cv2.imread(imgfile,cv2.IMREAD_GRAYSCALE)
-    array = np.asarray(img)
-    array = np.reshape(array,SIZE)
+        offset = int(SIZE / NUM_TRACKS)
 
-    for n in range(NUM_TRACKS):
+        # create midifile
+        midi = MidiFile()
 
-        # create midi track
-        track = MidiTrack()
-        midi.tracks.append(track)
-        midi.ticks_per_beat = MIN_NOTE
+        img = cv2.imread(imgfile,cv2.IMREAD_GRAYSCALE)
+        array = np.asarray(img)
+        array = np.reshape(array,SIZE)
 
-        # start song
-        track.append(MetaMessage("set_tempo",tempo=500000,time=0))
-        # set instrument
-        track.append(Message('program_change', program=87, time=0))
-        track.append(Message('control_change', channel=0, control=10, value=64, time=0))
+        for n in range(NUM_TRACKS):
 
-        currentNote = 0
-        count = 0
-        for i in range(offset*n,offset*(n+1),1):
+            # create midi track
+            track = MidiTrack()
+            midi.tracks.append(track)
+            midi.ticks_per_beat = MIN_NOTE
 
-            # if the current spot in the array is a new note
-            if array[i] != currentNote:
+            # start song
+            track.append(MetaMessage("set_tempo",tempo=500000,time=0))
+            # set instrument
+            track.append(Message('program_change', program=87, time=0))
+            track.append(Message('control_change', channel=0, control=10, value=64, time=0))
 
-                # append the previous note
-                if currentNote == 0: # the preceeding note was a rest
-                    track.append(Message("note_on",velocity=100,note=array[i],time=count))
-                else: # the preceeding note was another note
-                    track.append(Message("note_on",velocity=0,note=currentNote,time=count))
-                    if array[i] != 0:
-                        track.append(Message("note_on",velocity=100,note=array[i],time=0))
+            currentNote = 0
+            count = 0
+            for i in range(offset*n,offset*(n+1),1):
 
-                # update the current note and count
-                currentNote = array[i]
-                count = 0
-            else:
-                count += 1
+                # if the current spot in the array is a new note
+                if array[i] != currentNote:
 
-    # save out to midi
-    midi.save(midifile)
+                    # append the previous note
+                    if currentNote == 0: # the preceeding note was a rest
+                        track.append(Message("note_on",velocity=100,note=array[i],time=count))
+                    else: # the preceeding note was another note
+                        track.append(Message("note_on",velocity=0,note=currentNote,time=count))
+                        if array[i] != 0:
+                            track.append(Message("note_on",velocity=100,note=array[i],time=0))
+
+                    # update the current note and count
+                    currentNote = array[i]
+                    count = 0
+                else:
+                    count += 1
+
+        # save out to midi
+        midi.save(midifile)
+
+    except:
+
+        print(f"Failed to convert {imgfile}!")
 
 def midiToPng(song,image):
 
-    array = np.zeros(SIZE,np.uint8)
+    print(f"Converting {song} to {image}")
 
-    midi = MidiFile(song)
+    try:
 
-    offset = int(SIZE / NUM_TRACKS)
+        total = 0
 
-    n = 0
-    for midiTrack in midi.tracks:
+        array = np.zeros(SIZE,np.uint8)
 
-        if len(midiTrack) > 24:
+        midi = MidiFile(song)
 
-            track = [t for t in reversed(midiTrack)]
+        offset = int(SIZE / NUM_TRACKS)
 
-            i = offset * n # pointer to array index
-            lim = offset * (n + 1)
-            curNote = 0
-            count = 0 # duration of current note
-            lastCtrl = False # if the last message was a control message
-            while len(track) > 0 and i < lim:
+        n = 0
+        for midiTrack in midi.tracks:
 
-                message = track.pop()
-                # print(message)
+            if len(midiTrack) > 24:
 
-                if not message.is_meta:
+                track = [t for t in reversed(midiTrack)]
 
-                    # get message time
-                    t = message.time
-                    count += t
+                i = offset * n # pointer to array index
+                lim = offset * (n + 1)
+                curNote = 0
+                count = 0 # duration of current note
+                lastCtrl = False # if the last message was a control message
+                while len(track) > 0 and i < lim:
 
-                    # message type is note on
-                    if message.type == "note_on":
+                    message = track.pop()
+                    # print(message)
 
-                        v = message.velocity
+                    if not message.is_meta:
 
-                        if v != 0: # if note on
-                            if t == 0: # if note is instant
-                                curNote = message.note
-                            else: # if note is not instant
-                                i += t + 1
+                        # get message time
+                        t = message.time
+                        count += t
+
+                        # message type is note on
+                        if message.type == "note_on":
+
+                            v = message.velocity
+
+                            if v != 0: # if note on
+                                if t == 0: # if note is instant
+                                    curNote = message.note
+                                else: # if note is not instant
+                                    i += t + 1
+                                    count = 0
+                            else: # note off
+                                array[i:i+count+1] = curNote
+                                total += curNote * count
+                                i += count + 1
                                 count = 0
-                        else: # note off
-                            array[i:i+count+1] = curNote
-                            i += count + 1
-                            count = 0
 
-            # iterate
-            n += 1
+                # iterate
+                n += 1
 
-    array = np.reshape(array, (DIM, DIM))
-    cv2.imwrite(image,array)
+        if total > MIN_VALUE:
+            array = np.reshape(array, (DIM, DIM))
+            cv2.imwrite(image,array)
+        else: 
+            print("Song was too short!")
+
+    except:
+        print(f"Failed to convert {song}")
+
 
 if __name__ == "__main__":
 
+    args=sys.argv
+    globals()[args[1]](*args[2:])
 
-    midiToPng("new_song.mid","song.png")
-    pngToMidi("song.png","png_song.mid")
+    # midiToPng("new_song.mid","song.png")
+    # pngToMidi("song.png","png_song.mid")
     
 
         
